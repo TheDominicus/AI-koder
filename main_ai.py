8d51e6f16fcab0b8ab7a661b58e53d1888ca49bb
Dominik Šuráni <xsurani@stuba.sk>
2024-08-03 14:17:41 +0200

    Update main_ai.py
    Created simple communication with OPEN AI

--- main_ai.py
+++ main_ai.py
@@ -12,13 +12,33 @@ key = os.getenv('KEY')
 # init OPEN AI with langchain
 llm = OpenAI(api_key = key) 
 
-st.title("OPEN AI chat model")
+# chat history - logging our messages
+if 'chat_history' not in st.session_state:
+    st.session_state.chat_history = []
 
+# template how communication should look
 prompt = PromptTemplate(
     input_variables=["question"],
     template="Question: {question}\nAnswer: "
 )
 
+# answer the question in style of our prompt-template (from understand question to give answer)
+# LLMChain is like workgroup 
 chain = LLMChain(llm = llm, prompt = prompt)
 
+# FRONTEND - title
+st.title("OPEN AI chat model")
+
+# input line
 user_chat = st.chat_input("Your question...")
+
+if user_chat is not None and user_chat != "":
+    st.session_state.chat_history.append(user_chat)
+
+    with st.chat_message("User"):
+        # formated output
+        st.markdown(user_chat)
+
+    with st.chat_message("AI"):
+        ai_answer = chain.run(question = user_chat)
+        st.markdown(ai_answer)
